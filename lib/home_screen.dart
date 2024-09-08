6e515f04a5199473fe9e620bf3c09542f7ef95c3
luigi.micco <luigi.micco@gmail.com>
2024-09-08 18:47:43 +0000

    [feat] input field and go button

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -14,10 +14,35 @@ class _homescreenState extends State<homescreen> {
     String query = _controller.text.trim();
     Uri? uri = Uri.tryParse(query);
     bool isUrl = Uri.parse(query).isAbsolute;
+
+    if (!isUrl) {
+      query= 'http://www.google.com?q=$query';
+    }
+    // Navigator.push(context, MaterialPageRoute(builder: (context) => MyWebView(url: query)));
   }
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar:  AppBar(
+        title: const Text("Web view"),
+      ),
+      body: Padding(padding: const EdgeInsets.all(15),
+      child: Column(mainAxisAlignment: MainAxisAlignment.center, crossAxisAlignment: CrossAxisAlignment.center, children: [
+        TextFormField(controller: _controller,
+        decoration: const InputDecoration(
+          hintText: "Enter URL or SEARCH",
+          border: OutlineInputBorder(),
+          suffixIcon: Icon(Icons.search)
+        ),),
+
+        const SizedBox(height: 30,),
+        SizedBox(width: 200, height: 50,
+        child: 
+        ElevatedButton(onPressed: searchOrNavigate, child: Text("GO", style: TextStyle(fontSize: 18),),
+        style: ElevatedButton.styleFrom(backgroundColor: Colors.indigo, foregroundColor: Colors.white),)
+    ),],),),
+
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import './home_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,8 +11,6 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: homescreen()
-    );
+        debugShowCheckedModeBanner: false, home: homescreen());
   }
 }
