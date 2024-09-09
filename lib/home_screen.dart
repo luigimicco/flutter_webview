a67e2e7ad63422a75a3d40ce5dd48ac191fae25b
luigi.micco <luigi.micco@gmail.com>
2024-09-09 18:32:34 +0000

    [fix]

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -2,14 +2,14 @@ import 'package:flutter/material.dart';
 
 import 'mywebview.dart';
 
-class homescreen extends StatefulWidget {
-  const homescreen({super.key});
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
 
   @override
-  State<homescreen> createState() => _homescreenState();
+  State<HomeScreen> createState() => _HomeScreenState();
 }
 
-class _homescreenState extends State<homescreen> {
+class _HomeScreenState extends State<HomeScreen> {
   final TextEditingController _controller = TextEditingController();
 
   void searchOrNavigate() {
@@ -18,33 +18,50 @@ class _homescreenState extends State<homescreen> {
     bool isUrl = Uri.parse(query).isAbsolute;
 
     if (!isUrl) {
-      query= 'http://www.google.com?q=$query';
+      query = 'https://www.google.com/search?q=$query';
     }
-    Navigator.push(context, MaterialPageRoute(builder: (context) => MyWebView(url: query)));
+    Navigator.push(context,
+        MaterialPageRoute(builder: (context) => MyWebView(url: query)));
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar:  AppBar(
+      appBar: AppBar(
         title: const Text("Web view"),
       ),
-      body: Padding(padding: const EdgeInsets.all(15),
-      child: Column(mainAxisAlignment: MainAxisAlignment.center, crossAxisAlignment: CrossAxisAlignment.center, children: [
-        TextFormField(controller: _controller,
-        decoration: const InputDecoration(
-          hintText: "Enter URL or SEARCH",
-          border: OutlineInputBorder(),
-          suffixIcon: Icon(Icons.search)
-        ),),
-
-        const SizedBox(height: 30,),
-        SizedBox(width: 200, height: 50,
-        child: 
-        ElevatedButton(onPressed: searchOrNavigate,
-        style: ElevatedButton.styleFrom(backgroundColor: Colors.indigo, foregroundColor: Colors.white), child: const Text("GO", style: TextStyle(fontSize: 18),),)
-    ),],),),
-
+      body: Padding(
+        padding: const EdgeInsets.all(15),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            TextFormField(
+              controller: _controller,
+              decoration: const InputDecoration(
+                  hintText: "Enter URL or SEARCH",
+                  border: OutlineInputBorder(),
+                  suffixIcon: Icon(Icons.search)),
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            SizedBox(
+                width: 200,
+                height: 50,
+                child: ElevatedButton(
+                  onPressed: searchOrNavigate,
+                  style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.indigo,
+                      foregroundColor: Colors.white),
+                  child: const Text(
+                    "GO",
+                    style: TextStyle(fontSize: 18),
+                  ),
+                )),
+          ],
+        ),
+      ),
     );
   }
 }
